82b1a117c5e72db7a034e165ebe15647d5452d91
ddurumboy2197 <ddurumboy2197@gmail.com>
2026-04-19 20:15:52 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,15 @@
+import pprint
+
+d = {
+    "name": "John",
+    "age": 30,
+    "city": "New York",
+    "hobbies": ["reading", "swimming", "coding"],
+    "address": {
+        "street": "123 Main St",
+        "apartment": 4,
+        "country": "USA"
+    }
+}
+
+pprint.pprint(d)
